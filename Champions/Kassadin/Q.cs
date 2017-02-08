074ab709ee387af76d55fb6018db95b8ac632855
Wyyzapd <2417003090@qq.com>
2017-02-08 19:39:17 +0800

    Q.cs
    Lowered the size of spell scripts and added owner variable in Passive.

--- Champions/Kassadin/Q.cs
+++ Champions/Kassadin/Q.cs
@@ -1,4 +1,5 @@
 using LeagueSandbox.GameServer.Logic.GameObjects;
+using LeagueSandbox.GameServer.Logic.API;
 
 namespace Kassadin
 {
@@ -6,22 +7,27 @@ namespace Kassadin
     {
         public static void OnStartCasting(Champion owner, Spell spell, Unit target)
         {
-
+            ApiFunctionManager.AddParticleTarget(owner, "kassadin_base_q_cas.troy", owner, 1, "L_HAND");
         }
 
         public static void OnFinishCasting(Champion owner, Spell spell, Unit target)
         {
-
+           spell.AddProjectileTarget("NullLance", target, true);    
         }
 
         public static void ApplyEffects(Champion owner, Unit target, Spell spell, Projectile projectile)
-        {
-
+        {        
+            var ap = owner.GetStats().AbilityPower.Total * 0.8f;
+            var damage = 30 + spell.Level * 50 + ap;
+               if (target != null && !ApiFunctionManager.IsDead(target))
+            {
+                owner.DealDamageTo(target, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
+            }
         }
-
+        projectile.setToRemove();
+        
         public static void OnUpdate(double diff)
         {
 
         }
     }
-}
